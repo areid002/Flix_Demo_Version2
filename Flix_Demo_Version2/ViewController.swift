df812c5a5a51fdbc9564bc308a76e6dda9760f9b
Annmarie Reid <annmariereid@annmarie-reids-macbook-pro.local>
2018-10-06 06:21:07 -0400

    Initial Commit

---
+++ Flix_Demo_Version2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_Demo_Version2
+//
+//  Created by Annmarie Reid on 10/6/18.
+//  Copyright © 2018 Annmarie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
